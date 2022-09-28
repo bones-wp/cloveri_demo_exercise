b162ceb62943b8b19015abf8adf915cc020ff374
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-09-29 00:43:47 +0300

    Initial commit

---
+++ src/main/java/com/cloveri/demo_exercise/DemoExerciseApplication.java
@@ -0,0 +1,14 @@
+package com.cloveri.demo_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExerciseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Cloveri!");
+		SpringApplication.run(DemoExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloveri/demo_exercise/DemoExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloveri.demo_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
